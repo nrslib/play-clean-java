b4ccde99a99d802806b1736f7d84ad786c4c9d94
srn <masanobu.naruse@gmail.com>
2019-05-02 14:49:25 +0900

    change first page

--- playapp/app/controllers/Application.java
+++ playapp/app/controllers/Application.java
@@ -9,12 +9,8 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-    @Inject
-    UseCaseBus bus;
-
     public Result index()
     {
-        UserGetListOutputData outputData = bus.handle(new UserGetListInputData());
         return ok(index.render("Your new application is ready."));
     }
 }
